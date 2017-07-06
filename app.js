e1b5cfb41a596b162f1fd55e43433bee92528740
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-06 19:12:16 +0200

    Added more comments; added command interacting with existing data.

--- app.js
+++ app.js
@@ -10,25 +10,49 @@ var message_welcome = new Message().text('My Lord, I\'ll be at your service!');
 
 // Listens for /start command. Notice that you don't write slash as written inside official documentation.
 bot.command('start', function(message){
-  let welcome = message_welcome.to(message.chat.id);
-  bot.send(welcome);
+  message_welcome.to(message.chat.id);  // With to method, you can choose which chat Telegram Bot has to answer to.
+  bot.send(message_welcome);  // Send the message over Telegram API.
 });
 
 // Listens for a regexp (In this case, a phrase containing Hello, Ciao or Hi in any position).
 // Notice: case insensitive with final 'i' in regexp. If you don't add it, it listens words only if first char is capital letter.
 bot.get(/Hello|Ciao|Hi/i, function(message) {
-  let answer = new Message()
+  let message_hello = new Message() // This is how you can write a non-cached Message object.
     .text('Hello, my Lord.')
-    .to(message.chat.id); 
-  bot.send(answer);
+    .to(message.chat.id);
+  bot.send(message_hello);
 });
 
 // Listens for command 'praise', with required parameter <people>, with optional parameter [details].
+// Notice: since details parameter is optional, it can be null. You might want to check if it's null.
 bot.command('praise <people> [details]', function(message) {
-  var answer = new Message()
-    .text("I bow to you, "+message.args.people+", "+message.args.details+"!")
+  let extra_details;  // String to contact to the message.
+  if(message.args.details != null) extra_details=", "+message.args.details+"!"; // Add extra text if details argument is here.
+  else                             extra_details="";
+  let message_praise = new Message()
+    .text("I bow to you, "+message.args.people+extra_details)
     .to(message.chat.id);
+  bot.send(message_praise);
 });
 
+// It's also possible to communicate with Node.js app to change variable values such as temperature.
+// Think so far: you might want to deal with MongoDB/MySQL database to store and read data!
+// Notice: <param> guarantees that this required parameter is set by user input.
+var temperature = 20; // For this example, I use default value.
+bot.command('temperature <param>', function(message){
+  let message_params_text;
+  if(isFinite(message.args.param)){
+    message_params_text = 'Temperature before: '+temperature+', after: '+message.args.param;
+    temperature = message.args.param; // Store new value inside temperature var.
+  }else{
+    message_params_text = 'Error: the temperature you\'ve inserted is not a valid number.';
+  }
 
+  let test = new Message()
+    .text(message_params_text)
+    .to(message.chat.id);
+  bot.send(test);
+});
+
+// Log in web-server console that Telegram Bot is started.
 console.log("[SERVER] Bot started.");
