e8fccb44d23341f3559179407e3f0b056a46bc2e
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-07 15:21:56 +0200

    Added comments about babel-core/register.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-//require('babel-core/register');
+/*
+    NOTICE: use the following require if you want to avoid build/serve process.
+    NOT FOR PRODUCTION-USE BECAUSE IT ADDS 1-2 SECONDS MORE TO NODE.JS STARTUP!
+*/
+// require('babel-core/register');
+/*
+    For production environment, run Node.js with "npm run build" (If you edited files)
+    and "npm run serve" (To execute the server scripts) for best performances.
+*/
+
+// Our application starting point.
 require('./app.js');
